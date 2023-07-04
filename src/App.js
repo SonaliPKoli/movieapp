031328b6a822085064d1940b67cf6efabab6dd89
SonaliPKoli <sonalikoli2002@gmail.com>
2023-07-04 15:21:00 +0530

    movie app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+// 338fa8b
 
-function App() {
+import SearchIcon from "./search.svg";
+import "./App.css";
+import { MovieCard } from "./MovieCard";
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=338fa8b";
+export const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState([]);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("spiderman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="search for movies"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(search)} />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/MovieCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+export const MovieCard = ({movie}) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.year}</p>
+      </div>
+      <div>
+        <img
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt="img"
+        />
+      </div>
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+  );
+};
